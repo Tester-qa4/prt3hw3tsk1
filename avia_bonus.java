4fa78b5c220824ed789a80a51e6852f68bb19eca
tester <mgnfirter@gmail.com>
2020-03-10 23:18:55 +0400

    add avia_bonus

---
+++ avia_bonus.java
@@ -0,0 +1,9 @@
+public class avia_bonus {
+    public static void main(String[] args) {
+        int coast = (int)2221f;
+        int prc_bonus = 20;
+        int miles= coast / prc_bonus;
+        System.out.println(miles);
+
+    }
+}
