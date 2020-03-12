7e4cae627844290254ca75305544d9320fc119af
tester <mgnfirter@gmail.com>
2020-03-12 19:54:23 +0400

    fix code

---
+++ aviaBonus.java
@@ -0,0 +1,9 @@
+public class aviaBonus {
+    public static void main(String[] args) {
+        int cost = (int) 2221f;
+        int prcBonus = 20;
+        int miles = cost / prcBonus;
+        System.out.println(miles);
+
+    }
+}
--- avia_bonus.java
+++
@@ -1,9 +0,0 @@
-public class avia_bonus {
-    public static void main(String[] args) {
-        int coast = (int)2221f;
-        int prc_bonus = 20;
-        int miles= coast / prc_bonus;
-        System.out.println(miles);
-
-    }
-}
